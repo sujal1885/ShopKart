fec2a9a78b12a62b6c054c90ef5444948764d80a
sujal1885 <sujalagrawalgondia@gmail.com>
2024-03-28 22:06:56 +0530

    created shopkart backend

---
+++ frontend/src/component/Product/ReviewCard.js
@@ -0,0 +1,23 @@
+import ReactStars from "react-rating-stars-component"
+import React from "react";
+import profilePng from "../../images/Profile.png";
+
+const ReviewCard = ({ review }) => {
+    const options = {
+        size: "large",
+        value: review.ratings,
+        readOnly: true,
+        precision: 0.5,
+      };
+
+  return (
+    <div className="reviewCard">
+      <img src={profilePng} alt="User" />
+      <p>{review.name}</p>
+      <ReactStars {...options} />
+      <span className="reviewCardComment">{review.comment}</span>
+    </div>
+  );
+};
+
+export default ReviewCard;
