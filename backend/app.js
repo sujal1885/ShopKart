71868ea7ade84c16a4dc810f5d739403054150ae
sujal1885 <sujalagrawalgondia@gmail.com>
2024-06-04 00:51:17 +0530

    made one server of two

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
+const path = require("path");
 
 
 
@@ -30,6 +31,12 @@ app.use("/api/v1",product);
 app.use("/api/v1",user);
 app.use("/api/v1",order);
 app.use("/api/v1",payment);
+
+app.use(express.static(path.join(__dirname,"../frontend/build")));
+
+app.get("*",(req,res) => {
+    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
+})
 
 // Middleware for Errors
 app.use(errorMiddleware);
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const app = require("./app")
 const dotenv = require("dotenv");
 const cloudinary = require("cloudinary")
 const connectDatabase = require("./config/database");
+const express = require("express");
+
 
 
 // Handling Uncaught Exception
@@ -23,6 +25,8 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+
 
 const server = app.listen(process.env.PORT,()=> {
     console.log(`Server is working on http://localhost:${process.env.PORT}`)
